529efb3f96bd5dec6aaa1789cb5a5d35992dbcdd
manager <manager@qq.com>
2020-05-28 15:35:54 +0800

    create table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+     id = models.AutoField(primary_key=True, verbose_name='主键自增')
+     username = models.CharField(max_length=12, verbose_name='用户名')
+     password = models.CharField(max_length=36, verbose_name='密码')
+     # is_active = models.BooleanField(default=False, verbose_name='激活状态')
+
+     def __str__(self):
+        return "<{}>".format(self.username)
+
+     class Meta:
+        db_table = "UserProfile"
+
+
+
