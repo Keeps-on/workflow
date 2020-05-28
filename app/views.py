4c7e02cbfec6aed062d55ae9061f1b0100f00441
dev-lee <dev-lee@qq.com>
2020-05-28 16:05:17 +0800

    json

--- app/views.py
+++ app/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 # Create your views here.
 from app.models import *
-
+import json
 def index(request):
     user=UserProfile.objects.create(username='lee',password='123456')
-    all=UserProfile.objects.all().values()
+    all=UserProfile.objects.all().values('username','password')
     print(all)
-    return HttpResponse('this is dev-lee')
+    res=list(all)
+    return HttpResponse(json.dumps(res))
